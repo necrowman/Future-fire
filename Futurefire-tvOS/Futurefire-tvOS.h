0d5843beead3425d3c6ca6605fde14a7ddd6a834
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-07-07 14:38:33 +0300

    added tvOS framework target;

---
+++ Futurefire-tvOS/Futurefire-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Futurefire-tvOS.h
+//  Futurefire-tvOS
+//
+//  Created by Ruslan Yupyn on 7/7/16.
+//
+//
+
+//#import <UIKit/UIKit.h>
+
+//! Project version number for Futurefire-tvOS.
+FOUNDATION_EXPORT double Futurefire_tvOSVersionNumber;
+
+//! Project version string for Futurefire-tvOS.
+FOUNDATION_EXPORT const unsigned char Futurefire_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Futurefire_tvOS/PublicHeader.h>
+
+
