375584e6a0896da8d9c8b2b85df57f76ee52077c
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-07-07 15:43:59 +0300

    added Mac target for framework;

---
+++ Futurefire-Mac/Futurefire-Mac.h
@@ -0,0 +1,19 @@
+//
+//  Futurefire-Mac.h
+//  Futurefire-Mac
+//
+//  Created by Ruslan Yupyn on 7/7/16.
+//
+//
+
+//#import <Cocoa/Cocoa.h>
+
+//! Project version number for Futurefire-Mac.
+FOUNDATION_EXPORT double Futurefire_MacVersionNumber;
+
+//! Project version string for Futurefire-Mac.
+FOUNDATION_EXPORT const unsigned char Futurefire_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Futurefire_Mac/PublicHeader.h>
+
+
